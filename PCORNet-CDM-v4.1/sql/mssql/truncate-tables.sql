57b62747c17d0815600763dbce4e9994448ccc45
Jamie Estill <jaestill@med.umich.edu>
2018-05-18 14:54:57 -0400

    truncate tables for cdm 4.1

---
+++ PCORNet-CDM-v4.1/sql/mssql/truncate-tables.sql
@@ -0,0 +1,31 @@
+-- PCORnet Common Data Model v4.1
+-- Model 2018-05-15
+
+-- SCHEMA   : PCORI_CDMV4.1
+-- AUTHORS  : Ehsanullah Jan, Jamie Estill
+-- UPDATED  : MAY 18, 2018
+
+-- LICENSE  : Apache License v 2.0
+--            http://www.apache.org/licenses/LICENSE-2.0
+
+-- TRUNCATE TABLES FROM PCORI CDM 4.1
+
+TRUNCATE TABLE pcori_cdmv4_1.demographic;
+TRUNCATE TABLE pcori_cdmv4_1.enrollment;
+TRUNCATE TABLE pcori_cdmv4_1.encounter;
+TRUNCATE TABLE pcori_cdmv4_1.diagnosis;
+TRUNCATE TABLE pcori_cdmv4_1.procedures;
+TRUNCATE TABLE pcori_cdmv4_1.vital;
+TRUNCATE TABLE pcori_cdmv4_1.dispensing;
+TRUNCATE TABLE pcori_cdmv4_1.lab_result_cm;
+TRUNCATE TABLE pcori_cdmv4_1.condition;
+TRUNCATE TABLE pcori_cdmv4_1.pro_cm;
+TRUNCATE TABLE pcori_cdmv4_1.prescribing;
+TRUNCATE TABLE pcori_cdmv4_1.pcornet_trial;
+TRUNCATE TABLE pcori_cdmv4_1.death;
+TRUNCATE TABLE pcori_cdmv4_1.death_cause;
+TRUNCATE TABLE pcori_cdmv4_1.med_admin;
+TRUNCATE TABLE pcori_cdmv4_1.provider;
+TRUNCATE TABLE pcori_cdmv4_1.obs_clin;
+TRUNCATE TABLE pcori_cdmv4_1.obs_gen;
+TRUNCATE TABLE pcori_cdmv4_1.harvest;
