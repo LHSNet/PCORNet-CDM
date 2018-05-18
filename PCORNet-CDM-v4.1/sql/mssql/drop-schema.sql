45adfccaaecf773157ae4c69fe2db6a23ce3afc4
Jamie Estill <jaestill@med.umich.edu>
2018-05-18 14:45:17 -0400

    drop schema for cdm 4.1

---
+++ PCORNet-CDM-v4.1/sql/mssql/drop-schema.sql
@@ -0,0 +1,31 @@
+-- PCORnet Common Data Model v4.1
+-- Model 2018-05-15
+
+-- SCHEMA   : PCORI_CDMV4.1
+-- AUTHORS  : Ehsanullah Jan, Jamie Estill
+-- UPDATED  : MAY 18, 2018
+
+-- LICENSE  : Apache License v 2.0
+--            http://www.apache.org/licenses/LICENSE-2.0
+
+-- DROP TABLES FROM PCORI CDM 4.1
+
+DROP TABLE pcori_cdmv4_1.demographic;
+DROP TABLE pcori_cdmv4_1.enrollment;
+DROP TABLE pcori_cdmv4_1.encounter;
+DROP TABLE pcori_cdmv4_1.diagnosis;
+DROP TABLE pcori_cdmv4_1.procedures;
+DROP TABLE pcori_cdmv4_1.vital;
+DROP TABLE pcori_cdmv4_1.dispensing;
+DROP TABLE pcori_cdmv4_1.lab_result_cm;
+DROP TABLE pcori_cdmv4_1.condition;
+DROP TABLE pcori_cdmv4_1.pro_cm;
+DROP TABLE pcori_cdmv4_1.prescribing;
+DROP TABLE pcori_cdmv4_1.pcornet_trial;
+DROP TABLE pcori_cdmv4_1.death;
+DROP TABLE pcori_cdmv4_1.death_cause;
+DROP TABLE pcori_cdmv4_1.med_admin;
+DROP TABLE pcori_cdmv4_1.provider;
+DROP TABLE pcori_cdmv4_1.obs_clin;
+DROP TABLE pcori_cdmv4_1.obs_gen;
+DROP TABLE pcori_cdmv4_1.harvest;
